0f4bdad38716db9ce70ef317843a42bdf955786c
kajsap <kajsaberglofmorales@gmail.com>
2026-09-22 21:47:04 +0100

    added the timeline

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import folium
+from folium.plugins import TimestampedGeoJson
+import json
 
 def load_data():
     data = pd.read_csv("puffin_data.csv")
@@ -29,7 +31,7 @@ def assign_colours(data):
     puffin_colours = {}
 
     for i, puffin_id in enumerate(puffins):
-        puffin_colours[puffin_id] = colours[i % len(colours)]
+        puffin_colours[puffin_id] = colours[i % len(colours)] #colours to repeat
 
     return puffin_colours
 
@@ -39,60 +41,152 @@ def create_map(data):
 
     map = folium.Map(
         location=[centre_lat, centre_long],
-        zoom_start= 3
+        zoom_start= 2.5
     )
 
     return map
 
-def group_data(data, map, puffin_colours):
-    puffin_groups = {}
+def create_timeline(data, map, puffin_colours):
 
-    for puffin_id, puffin_data in data.groupby("tag-local-identifier"):
-        colour = puffin_colours[puffin_id]
-        group = folium.FeatureGroup(
-            name=puffin_id,
-            show=False
-        )
+    #store the data for javascript contains all the geographical points of each puffin
+    puffin_data = {}
 
-        for _, row in puffin_data.iterrows():
-            folium.CircleMarker(
-                location=[row["location-lat"], row["location-long"]],
-                fill_opacity = 1,
-                fill = True,
-                radius=5,
-                fill_color = colour,
-                color = colour
-            ).add_to(group)
+    for puffin_id, group in data.groupby("tag-local-identifier"):
+        features = []
 
-        group.add_to(map)
+        for _, row in group.iterrows():
+            colour = puffin_colours[puffin_id]
 
-        puffin_groups[puffin_id] = group
+            feature = {
+                "type": "Feature",
 
-    return puffin_groups
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [row["location-long"], row["location-lat"]]
+                },
 
+                "properties": {
+                    "time": row["timestamp"].isoformat(),
+                    "puffin_id": str(puffin_id),
+                    "popup": str(puffin_id),
+                    "icon": "circle",
 
-def checkbox(puffin_groups, puffin_colours, map):
-    legend_items = ""
+                    "iconstyle": {
+                        "color": colour,
+                        "fillColor": colour,
+                        "fillOpacity": 1,
+                        "radius": 5
+                    }
+                }
+            }
+
+            features.append(feature)
+
+        puffin_data[str(puffin_id)] = features
+
+    #empty timeline
+    empty_geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    timeline = TimestampedGeoJson(
+        empty_geojson,
+        period="P1D",
+        add_last_point=True,
+        auto_play=False,
+        loop=False,
+        max_speed=2,
+        loop_button=True,
+        date_options="YYYY-MM-DD",
+        time_slider_drag_update=True
+    )
+
+    timeline.add_to(map)
+
+    timeline_variable = timeline.get_name()
     map_variable = map.get_name()
 
+    #convert python dict into javascript
+    puffin_data_json = json.dumps(puffin_data)
+
+    javascript = f"""
+    <script>
+
+    var puffinData = {puffin_data_json};
+
+    var selectedPuffins = {{}};
+
+    function togglePuffin(puffinID, checked) {{
+        selectedPuffins[puffinID] = checked;
+        updateTimeline();
+    }}
+
+    function updateTimeline(){{
+        var selectedFeatures = []; 
+
+        for (var puffinID in selectedPuffins){{
+        
+            if (selectedPuffins[puffinID] === true){{
+                var features = puffinData[puffinID];
+
+                if (features){{
+                    selectedFeatures = selectedFeatures.concat(features);
+                }}
+            }}  
+        }}
+
+        var newGeoJSON = {{
+            "type": "FeatureCollection",
+            "features": selectedFeatures
+        }};
+
+        console.log("Selected puffin points:", selectedFeatures.length);
+
+        var timeLayer = {timeline_variable};
+
+        //if timeline exists then remove the current points and add the new points
+        if (timeLayer && timeLayer._baseLayer){{
+            timeLayer._baseLayer.clearLayers();
+
+            timeLayer._baseLayer.addData(newGeoJSON);
+        }}
+    }}
+
+    </script>
+    """
+
+    map.get_root().html.add_child(folium.Element(javascript))
+
+    return timeline
+
+
+def checkbox(puffin_colours, map):
+    legend_items = ""
+
     for puffin_id, colour in puffin_colours.items():
-        layer_variable =  puffin_groups[puffin_id].get_name()
 
         legend_items += f"""
         <label>
             <input type="checkbox"
-                onchange="togglePuffin('{layer_variable}', this.checked)">
-            <span style="color:{colour}"></span>
+                onchange="togglePuffin('{puffin_id}', this.checked)">
+            <span style="
+            display: inline-block;
+            width: 10px;
+            height: 10px;
+            background: {colour};
+            color:{colour}">
+            </span>
             {puffin_id}
         </label><br>
-    """
+        """
 
     checkbox_html = f"""
     <div style="
         position: fixed; 
         bottom: 200px; 
         left: 50px; 
-        width: 100px; 
+        width: 130px; 
         height: 210px; 
         border:2px solid grey; 
         z-index:9999; 
@@ -105,23 +199,6 @@ def checkbox(puffin_groups, puffin_colours, map):
         {legend_items}
 
     </div>
-
-    <script>
-
-    function togglePuffin(layerName, checked){{
-        var layer = window[layerName];
-        var map = {map_variable};
-
-        if (checked){{
-            map.addLayer(layer);
-        }}
-        else{{
-            map.removeLayer(layer);
-        }}
-        
-    }}
-    </script>
-
     """
 
     map.get_root().html.add_child(folium.Element(checkbox_html))
@@ -130,8 +207,8 @@ def main():
     data = load_data()
     puffin_colours = assign_colours(data)
     map = create_map(data)
-    puffin_groups = group_data(data, map, puffin_colours)
-    checkbox(puffin_groups, puffin_colours, map)
+    timeline = create_timeline(data, map, puffin_colours)
+    checkbox(puffin_colours, map)
     map.save("puffin_map.html")
 
 main()
